1bdce1aca31d003850ed2198d9aa689c591def83
Ryan Williams <ryanwilliamsdesktop@outlook.com>
2020-11-17 15:47:37 +0000

    rewrite

--- CryptographyPractical4Task1/main.cpp
+++ CryptographyPractical4Task1/main.cpp
@@ -10,6 +10,7 @@
 #include <algorithm>
 #include <cctype>
 #include <chrono>
+#include <unordered_map>
 
 std::string password;
 SHA1 checksum;
@@ -17,20 +18,38 @@ SHA1 checksum;
 char currentGuess[40] = "";
 char actualHash[40] = "";
 
+const int SizeOfPasswordSpace = 2176782336;
 
 std::string charSet("abcdefghijklmnopqrstuvwxyz0123456789");
 
 
-bool IsCorrectPassword(std::string guess)
+std::unordered_map<std::string, std::string> umap;
+
+
+//To crack a sha1
+//generate a possible password
+//
+
+
+std::string hashToCrack;
+std::string currentHash;
+
+std::string Hash(std::string input)
 {
-    checksum.update(guess);
-    std::string hash = checksum.final();
+    checksum.update(input);
+    return checksum.final();
+}
 
+bool IsCorrectPassword(std::string nextHash)
+{
+
+    return Hash(nextHash).compare(hashToCrack) == 1;
+
+
+    /*std::string hash = checksum.final();
     hash.copy(currentGuess, 40);
-
     std::cout << "About to try " << currentGuess << " actual password hash is " << actualHash << std::endl;
-
-    return std::strcmp(currentGuess, actualHash) == 0;
+    return std::strcmp(currentGuess, actualHash) == 0;*/
 
 }
 
@@ -59,9 +78,14 @@ int Generate(unsigned int length, std::string s)
 }
 
 
-void CrackHash(int passwordLength, std::string hashToCrack)
+void CrackHash(std::string hashToCrack, int passwordLength)
 {
-    std::cout << "\n" << "Attempting to crack " << hashToCrack <<"...\n";
+    //First check to see if its already in hash table
+
+    //if (umap[])
+
+
+    std::cout << "\n" << "Starting to brute force " << hashToCrack <<"...\n";
     auto start = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed;
 
@@ -91,37 +115,38 @@ void CrackHash(int passwordLength, std::string hashToCrack)
     }
 
     std::cout << "Failed to crack hash\n";
+
 }
 
 
 int main()
 {
 
+
     std::string intput;
     std::cout << "Enter sha1 hash or type test: ";
     getline(std::cin, intput);
     if (intput == "test")
     {
-        CrackHash(6, "0ade7c2cf97f75d009975f4d720d1fa6c19f4897"); //Mine that are quick
-        CrackHash(6, "a9993e364706816aba3e25717850c26c9cd0d89d"); //Mine that are quick
-        CrackHash(6, "589c22335a381f122d129225f5c0ba3056ed5811"); //Mine that are quick
-        CrackHash(6, "d8b1c3e59ae165abbbb80c3a017ce77d5e281b18"); //Mine that are quick
+        CrackHash("0ade7c2cf97f75d009975f4d720d1fa6c19f4897", 6); //Mine that are quick
+        CrackHash("a9993e364706816aba3e25717850c26c9cd0d89d", 6); //Mine that are quick
+        CrackHash("589c22335a381f122d129225f5c0ba3056ed5811", 6); //Mine that are quick
+        CrackHash("d8b1c3e59ae165abbbb80c3a017ce77d5e281b18", 6); //Mine that are quick
 
-
-        //CrackHash(6, "c2543fff3bfa6f144c2f06a7de6cd10c0b650cae"); //From worksheet
-        //CrackHash(6, "b47f363e2b430c0647f14deea3eced9b0ef300ce"); //From worksheet
-        CrackHash(6, "e74295bfc2ed0b52d40073e8ebad555100df1380"); //From worksheet
-        //CrackHash(6, "0f7d0d088b6ea936fb25b477722d734706fe8b40"); //From worksheet
-        //CrackHash(6, "77cfc481d3e76b543daf39e7f9bf86be2e664959"); //From worksheet
-        //CrackHash(6, "5cc48a1da13ad8cef1f5fad70ead8362aabc68a1"); //From worksheet
-        //CrackHash(6, "4bcc3a95bdd9a11b28883290b03086e82af90212"); //From worksheet
-        //CrackHash(6, "7302ba343c5ef19004df7489794a0adaee68d285"); //From worksheet
-        //CrackHash(6, "21e7133508c40bbdf2be8a7bdc35b7de0b618ae4"); //From worksheet
-        //CrackHash(6, "6ef80072f39071d4118a6e7890e209d4dd07e504"); //From worksheet
-        //CrackHash(6, "02285af8f969dc5c7b12be72fbce858997afe80a"); //From worksheet
-        //CrackHash(6, "57864da96344366865dd7cade69467d811a7961b"); //From worksheet
+        //CrackHash("c2543fff3bfa6f144c2f06a7de6cd10c0b650cae", 6); //From worksheet
+        //CrackHash("b47f363e2b430c0647f14deea3eced9b0ef300ce", 6); //From worksheet
+        //CrackHash("e74295bfc2ed0b52d40073e8ebad555100df1380", 6); //From worksheet
+        //CrackHash("0f7d0d088b6ea936fb25b477722d734706fe8b40", 6); //From worksheet
+        //CrackHash("77cfc481d3e76b543daf39e7f9bf86be2e664959", 6); //From worksheet
+        //CrackHash("5cc48a1da13ad8cef1f5fad70ead8362aabc68a1", 6); //From worksheet
+        //CrackHash("4bcc3a95bdd9a11b28883290b03086e82af90212", 6); //From worksheet
+        //CrackHash("7302ba343c5ef19004df7489794a0adaee68d285", 6); //From worksheet
+        //CrackHash("21e7133508c40bbdf2be8a7bdc35b7de0b618ae4", 6); //From worksheet
+        //CrackHash("6ef80072f39071d4118a6e7890e209d4dd07e504", 6); //From worksheet
+        //CrackHash("02285af8f969dc5c7b12be72fbce858997afe80a", 6); //From worksheet
+        //CrackHash("57864da96344366865dd7cade69467d811a7961b", 6); //From worksheet
     }
     else
-        CrackHash(6, intput);
+        CrackHash(intput, 6);
     
 }
