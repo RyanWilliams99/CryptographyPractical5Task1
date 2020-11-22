43b48dfc5fe8f719c90ae0e14eaab8a3a3b5972c
Ryan Williams <ryanwilliamsdesktop@outlook.com>
2020-11-22 11:35:36 +0000

    Additional logging

--- CryptographyPractical4Task1/main.cpp
+++ CryptographyPractical4Task1/main.cpp
@@ -5,6 +5,7 @@
 
 #include <ctype.h>      
 #include <iostream>     
+#include <fstream>
 #include <string>       
 #include <vector>    
 #include <math.h> 
@@ -23,6 +24,39 @@ char actualHash[40] = "";
 const int SizeOfPasswordSpace = 2176782336;
 
 std::string charSet("abcdefghijklmnopqrstuvwxyz0123456789");
+
+
+
+void logstr(std::string str, bool newline, bool prefixWithTime)
+{
+    
+    std::ofstream myfile;
+
+    myfile.open("output.txt", std::ios::out | std::ios::ios_base::app);
+
+    if (myfile.is_open())
+    {
+        if (prefixWithTime)
+        {
+            myfile << currentTime();
+            std::cout << str;
+        }
+        myfile << str;
+        std::cout << str;
+        if (newline)
+        {
+            myfile << "\n";
+            std::cout << "\n";
+        }
+            
+        myfile.close();
+    }
+    else
+    {
+        std::cerr << "didn't write" << std::endl;
+    }
+
+}
 
 
 //std::unordered_map<std::string, std::string> umap;
@@ -48,7 +82,7 @@ void testSHA1(const std::string& val) {
     s.getDigest(digest);
     char tmp[48];
     snprintf(tmp, 45, "%08x %08x %08x %08x %08x", digest[0], digest[1], digest[2], digest[3], digest[4]);
-    std::cout << "Calculated : (\"" << val << "\") = " << tmp << std::endl;
+    logstr("Calculated : (\"" + val + "\") = " + tmp, true);
 }
 
 //std::string newHash(std::string input)
@@ -62,18 +96,9 @@ void testSHA1(const std::string& val) {
 
 bool IsCorrectPassword(std::string nextHash)
 {
-
     std::string hash = Hash(nextHash);
-
     int result = hash.compare(hashToCrack);
-
     return result == 0;
-
-    /*std::string hash = checksum.final();
-    hash.copy(currentGuess, 40);
-    std::cout << "About to try " << currentGuess << " actual password hash is " << actualHash << std::endl;
-    return std::strcmp(currentGuess, actualHash) == 0;*/
-
 }
 
 int Generate(unsigned int length, std::string s)
@@ -85,7 +110,7 @@ int Generate(unsigned int length, std::string s)
         //std::cout << "Trying Password " << s;
         if (IsCorrectPassword(s))
         {
-            std::cout << "\nFound Correct Password \"" << s;
+            logstr("\nFound Correct Password \"" + s, false);
             return 1;
         }
 
@@ -104,7 +129,7 @@ int Generate(unsigned int length, std::string s)
     }
 }
 
-std::string current_time() {
+std::string currentTime() {
     time_t now = time(NULL);
     struct tm tstruct;
     char buf[40];
@@ -120,8 +145,8 @@ void CrackHash(std::string inputHash, int passwordLength)
 
     //if (umap[])
 
-
-    std::cout << "\n" << "Starting to brute force " << inputHash <<"...\n";
+    logstr("\nStarting to brute force " + inputHash + "...", true);
+    //std::cout << "\n" << "Starting to brute force " << inputHash <<"...\n";
     auto start = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed;
 
@@ -142,12 +167,15 @@ void CrackHash(std::string inputHash, int passwordLength)
     {
 
         std::time_t end_time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-        std::cout << current_time() << " - Trying all possible " << i << " length Passwords...";
+        
+
+        logstr( + "Trying all possible " + std::to_string(i) + " length Passwords...", false);
+
         if (Generate(i, "") == 1)
         {
             elapsed = std::chrono::high_resolution_clock::now() - start;
 
-            std::cout << "\" after " << elapsed.count() << " seconds (" << elapsed.count() / 60 << " Minutes)" << "\n";
+            logstr("\" after " + std::to_string(elapsed.count()) + " seconds (" + std::to_string(elapsed.count() / 60) + " Minutes)", true);
             return;
         }
         std::cout << "\n";
@@ -161,21 +189,16 @@ void CrackHash(std::string inputHash, int passwordLength)
 
 int main()
 {
-    //testSHA1("The quick brown fox jumps over the lazy dog");
-    //testSHA1("The quick brown fox jumps over the lazy cog");
-    //testSHA1("");
-
-
 
     std::string intput;
     std::cout << "Enter sha1 hash or type test: ";
     getline(std::cin, intput);
     if (intput == "test")
     {
-        //CrackHash("0ade7c2cf97f75d009975f4d720d1fa6c19f4897", 6); //Mine that are quick
-        //CrackHash("a9993e364706816aba3e25717850c26c9cd0d89d", 6); //Mine that are quick
-        //CrackHash("589c22335a381f122d129225f5c0ba3056ed5811", 6); //Mine that are quick
-        //CrackHash("d8b1c3e59ae165abbbb80c3a017ce77d5e281b18", 6); //Mine that are quick
+        CrackHash("0ade7c2cf97f75d009975f4d720d1fa6c19f4897", 6); //Mine that are quick
+        CrackHash("a9993e364706816aba3e25717850c26c9cd0d89d", 6); //Mine that are quick
+        CrackHash("589c22335a381f122d129225f5c0ba3056ed5811", 6); //Mine that are quick
+        CrackHash("d8b1c3e59ae165abbbb80c3a017ce77d5e281b18", 6); //Mine that are quick
 
         //CrackHash("c2543fff3bfa6f144c2f06a7de6cd10c0b650cae", 6); //From worksheet
         //CrackHash("b47f363e2b430c0647f14deea3eced9b0ef300ce", 6); //From worksheet
@@ -188,7 +211,7 @@ int main()
         //CrackHash("21e7133508c40bbdf2be8a7bdc35b7de0b618ae4", 6); //From worksheet
         //CrackHash("6ef80072f39071d4118a6e7890e209d4dd07e504", 6); //From worksheet
         //CrackHash("02285af8f969dc5c7b12be72fbce858997afe80a", 6); //From worksheet
-        CrackHash("57864da96344366865dd7cade69467d811a7961b", 6); //From worksheet
+        //CrackHash("57864da96344366865dd7cade69467d811a7961b", 6); //From worksheet
     }
     else
     {
